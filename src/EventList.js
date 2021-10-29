33fa4ef44fe5ca729bd40d2c1686d0a50326a21e
IamVoNha <ov_ahn@yahoo.com>
2021-10-29 01:35:42 -0400

    update eventlist.js

--- src/EventList.js
+++ src/EventList.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react';
 import Event from './Event';
 
 class EventList extends Component {
-  render() {
-    const { events } = this.props;
-    return (
-      <ul className="EventList">
-        {events.map(event =>
-          <li key={event.id}>
-            <Event event={event} />
-          </li>
-        )}
-      </ul>
-    );
-  }
+	render() {
+		return (
+			<ul className="EventList">
+				{this.props.events.map(event => 
+					<li key={event.id}>
+						<Event event={event} />
+					</li>
+				)}
+			</ul>
+		)
+	}
 }
 
 export default EventList;
