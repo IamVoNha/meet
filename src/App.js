a6ec160ef7be9e5b6bc432e2d5a4cd0a075de51d
IamVoNha <ov_ahn@yahoo.com>
2021-11-06 02:54:15 -0400

    Implemented Notification alert, Welcome screen, & Privacy policy - Task 4.8

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import { NotificationAlert } from './Alert';
+import WelcomeScreen from './WelcomeScreen';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from "./NumberOfEvents";
-import { getEvents, extractLocations } from './api';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import './nprogress.css';
 
 class App extends Component {
@@ -14,19 +16,42 @@ class App extends Component {
       locations: {},
       numberOfEvents: 32,
       currentLocation: "all",
+      notificationText: '',
+      showWelcomeScreen: undefined
     };
   }
-  componentDidMount() {
+  async componentDidMount() {
      this.mounted = true;
-     getEvents().then((events) => {
-       if (this.mounted) {
-         this.setState({
-           events: events.slice(0, this.state.numberOfEvents),
-           locations: extractLocations(events),
-         });
-       }
-     });
-   }
+
+      if (navigator.onLine === false) {
+       this.setState({
+         notificationText: 'It seems you are offline. The events may be out of date while offline. To make sure you are getting the up to date events, make sure you are connected to the internet!'
+       });
+    } else {
+       this.setState({
+         notificationText: ''
+      });
+    }
+
+    const accessToken = localStorage.getItem('access_token');
+  const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+  const searchParams = new URLSearchParams(window.location.search);
+
+  const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+
+    if ((code || isTokenValid) && this.mounted) {
+
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events),
+          });
+        }
+      });
+    }
+  }
   componentWillUnmount() {
     this.mounted = false;
   }
@@ -49,17 +74,24 @@ class App extends Component {
     this.updateEvents(this.state.currentLocation, this.state.numberOfEvents);
   };
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     return (
       <div className="App">
+        <NotificationAlert text={this.state.notificationText} />
+
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
         />
+
         <NumberOfEvents
           numberOfEvents={this.state.numberOfEvents}
           updateNumberOfEvents={this.updateNumberOfEvents}
         />
+
         <EventList events={this.state.events} />
+
+        <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken() }} />
       </div>
     );
   }
